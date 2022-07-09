55c7e0dca6fc6e8de7766c509befb550ad97960c
unknown <arongrhyz@gmail.com>
2022-07-09 08:31:33 +0800

    implemented home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Initial Project Implementation</h1>
+    <div className="home-container">
+      <div className="right-triangle"></div>
+      <div className="home-main-content">
+        <h1 className="banner-title-primary">Hello There!</h1>
+        <h3 className="banner-title-secondary">
+          I am Rhyz, an aspiring software engineer. A pleasure to meet you.
+        </h3>
+        <button className="call-to-action">Get in touch</button>
+      </div>
+      <div className="image-container"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
