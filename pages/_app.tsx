681b26b51ed3e8e0e90e480ae7be3ca2ed05b794
unknown <arongrhyz@gmail.com>
2022-07-09 08:30:56 +0800

    implemented side nav on the app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/index.scss";
+import type { AppProps } from "next/app";
+
+import AboutIcon from "../components/icons/AboutIcon";
+import WorksIcon from "../components/icons/WorksIcon";
+import HomeIcon from "../components/icons/HomeIcon";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <div className="main-container">
+      <div className="navbar">
+        <ul>
+          <li
+            style={{
+              backgroundColor:
+                router.pathname === "/about" ? "#2994F2" : "#D9D9D9",
+            }}
+          >
+            <Link href={"/about"}>
+              <AboutIcon isActive={router.pathname === "/about"} />
+            </Link>
+          </li>
+          <li
+            style={{
+              backgroundColor:
+                router.pathname === "/works" ? "#2994F2" : "#D9D9D9",
+            }}
+          >
+            <WorksIcon isActive={router.pathname === "/works"} />
+          </li>
+          <li
+            style={{
+              backgroundColor: router.pathname === "/" ? "#2994F2" : "#D9D9D9",
+            }}
+          >
+            <HomeIcon isActive={router.pathname === "/"} />
+          </li>
+        </ul>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
