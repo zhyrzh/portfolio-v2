bdd8739d991735a937533ab098104fde3ef7fb98
unknown <arongrhyz@gmail.com>
2022-07-11 16:10:48 +0800

    added home page background image and fixed unclickable nav items

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
 import "../styles/index.scss";
 import type { AppProps } from "next/app";
-
-import { useRouter } from "next/router";
 import Navbar from "../components/Navbar";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
-    <div className="main-container">
+    <div
+      className="main-container"
+      style={{
+        backgroundImage:
+          router.pathname === "/"
+            ? "linear-gradient( 0deg, rgba(238, 250, 255, 0.9), rgba(238, 250, 255, 0.9)), url(./home-background.avif)"
+            : "",
+      }}
+    >
       <Navbar />
       <Component {...pageProps} />
     </div>
