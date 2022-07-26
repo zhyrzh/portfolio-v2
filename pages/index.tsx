4b588d875a44c5b1a5e7b73485e67ddf32020e7a
unknown <arongrhyz@gmail.com>
2022-07-26 16:17:39 +0800

    added footer and header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
 import type { NextPage } from "next";
+import Featured from "../components/icons/Featured";
 
 const Home: NextPage = () => {
   return (
-    <div className="home">
-      <div className="home__background"></div>
-      <div className="home__content">
-        <h1 className="home__title">Hello, I am Rhyz a Web Developer</h1>
-        <h1 className="home__sub-title">
-          I create software products such as web page and web servers. A
-          pleasure to meet you!
-        </h1>
-        <button className="home__cta">Download CV</button>
-        <button className="home__cta">Contact Me</button>
-      </div>
-      <div className="home__image"></div>
-    </div>
+    <>
+      <header className="header">
+        <div className="header__circle"></div>
+        <h3 className="header__title">
+          Rhyz Arong | <span className="header__sub-title">Home</span>
+        </h3>
+      </header>
+
+      <footer className="footer">
+        <h3 className="footer__text">
+          <span>&copy;</span> Rhyz Arong 2022
+        </h3>
+      </footer>
+    </>
   );
 };
 
